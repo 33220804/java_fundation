f70cffe9abd7b50517cb5414c29b552001830fd7
zengmeiling <965961989@qq.com>
2018-07-21 14:38:31 +0800

    list 和set

---
+++ src/list和set的区别.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class list和set的区别 {
+
+	public static void main(String[] args) {
+		 List list=new ArrayList();
+	        list.add("a");
+	        list.add("b");
+	        list.add("a");
+	        Set set=new HashSet();
+	        set.add("a");
+	        set.add("b");
+	        set.add("a");
+	        System.out.println(list.size()+","+set.size()); 
+	}
+
+}
+/**
+ * 结论：
+ * 		List有序可重复，set有序不可重复
+ * 
+ */
